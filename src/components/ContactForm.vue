8b619d04ba19d0d34bfbb8e63edd0fa57f48cef6
mike <mike@beansmile.com>
2017-03-08 21:06:55 +0800

    let user can submit form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,35 +1,35 @@
 <template>
-    <form class="form-horizontal">
+    <form class="form-horizontal" action="http://localhost:8089/contacts" method="post" >
         <div class="form-group">
             <label class="control-label col-sm-2" for="email">
                 Email:
             </label>
             <div class="col-sm-10">
-                <input type="email" class="form-control" id="email" placeholder="Enter email"/>
+                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" />
             </div>
         </div>
         <div class="form-group">
-            <label class="control-label col-sm-2" for="pwd">
+            <label class="control-label col-sm-2">
                 First Name:
             </label>
             <div class="col-sm-10">
-                <input type="First Name" class="form-control" placeholder="Enter First Name"/>
+                <input type="First Name" class="form-control" placeholder="Enter First Name" name="first_name"/>
             </div>
         </div>
         <div class="form-group">
-            <label class="control-label col-sm-2" for="pwd">
+            <label class="control-label col-sm-2">
                 Last Name:
             </label>
             <div class="col-sm-10">
-                <input type="First Name" class="form-control" placeholder="Enter Last Name"/>
+                <input type="First Name" class="form-control" placeholder="Enter Last Name" name="last_name"/>
             </div>
         </div>
         <div class="form-group">
-            <label class="control-label col-sm-2" for="pwd">
+            <label class="control-label col-sm-2" >
                 Description:
             </label>
             <div class="col-sm-10">
-                <textarea class="form-control" rows="3" placeholder="Description">
+                <textarea class="form-control" rows="3" placeholder="Description" name="description">
                 </textarea>
             </div>
         </div>
