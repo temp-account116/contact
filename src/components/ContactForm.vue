95d8b0ded9c242a689c9f5bc30d5aaa0637f759d
mike <mike@beansmile.com>
2017-03-08 21:02:15 +0800

    implemented ContactForm

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <form class="form-horizontal">
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="email">
+                Email:
+            </label>
+            <div class="col-sm-10">
+                <input type="email" class="form-control" id="email" placeholder="Enter email"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="pwd">
+                First Name:
+            </label>
+            <div class="col-sm-10">
+                <input type="First Name" class="form-control" placeholder="Enter First Name"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="pwd">
+                Last Name:
+            </label>
+            <div class="col-sm-10">
+                <input type="First Name" class="form-control" placeholder="Enter Last Name"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="pwd">
+                Description:
+            </label>
+            <div class="col-sm-10">
+                <textarea class="form-control" rows="3" placeholder="Description">
+                </textarea>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-default">
+                    Submit
+                </button>
+            </div>
+        </div>
+    </form>
+  </template>
+<script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+// TODO: replace me with index 
+import ContactForm from '@/components/ContactForm'
+
 
 Vue.use(Router)
 
@@ -8,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'ContactForm',
+      component: ContactForm
     }
   ]
 })
